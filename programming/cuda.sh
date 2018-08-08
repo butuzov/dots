8b5bccb21c7ef6d2df0d0bb7e5ca4656f5b8fb82
Oleg Butuzov <butuzov@made.ua>
2018-08-08 11:41:49 +0300

    adding check for cuda existance ( just for macbook )

--- programming/cuda.sh
+++ programming/cuda.sh
@@ -1,5 +1,8 @@
-export CUDA_PATH="/Developer/NVIDIA/CUDA-9.1"
-export DYLD_LIBRARY_PATH="${CUDA_PATH}/lib:${DYLD_LIBRARY_PATH}"
-export PATH="${CUDA_PATH}/bin:${PATH}"
-export CPATH="${CUDA_PATH}/include/"
-export CGO_LDFLAGS="/usr/local/cuda/lib/libcuda.dylib ${CUDA_PATH}/lib/libcudart.dylib ${CUDA_PATH}/lib/libcublas.dylib ${CUDA_PATH}/lib/libcurand.dylib"
+CUDA_PATH="/Developer/NVIDIA/CUDA-9.1"
+if [[ -d $CUDA_PATH ]]; then
+    export CUDA_PATH="/Developer/NVIDIA/CUDA-9.1"
+    export DYLD_LIBRARY_PATH="${CUDA_PATH}/lib:${DYLD_LIBRARY_PATH}"
+    export PATH="${CUDA_PATH}/bin:${PATH}"
+    export CPATH="${CUDA_PATH}/include/"
+    export CGO_LDFLAGS="/usr/local/cuda/lib/libcuda.dylib ${CUDA_PATH}/lib/libcudart.dylib ${CUDA_PATH}/lib/libcublas.dylib ${CUDA_PATH}/lib/libcurand.dylib"
+fi
