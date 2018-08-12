3d62b7541ac4ef00cd3d1ca048d8dac7b8527425
Oleg Butuzov <butuzov@made.ua>
2018-08-12 09:12:34 +0300

    update bew

--- bash/locals.sh
+++ bash/locals.sh
@@ -4,3 +4,6 @@ alias vc='video && go run converter.go -directory=__converting'
 
 # Spinup wp-dev env
 alias web='/Volumes/Developer/WordPress/.settings/commander.sh $1 $(pwd)'
+
+# alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup'
+alias update='brew update; brew upgrade; brew cleanup;'
