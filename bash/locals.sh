bd647c6e23b815491eea0a74b65287c77be6a8f2
Oleg Butuzov <butuzov@made.ua>
2018-09-07 08:24:17 +0300

    Simpler LEMP Stack Deploy

--- bash/locals.sh
+++ bash/locals.sh
@@ -2,8 +2,20 @@
 alias video='cd /Volumes/Developer/Video-Library/Videos'
 alias vc='video && go run converter.go -directory=__converting'
 
-# Spinup wp-dev env
-alias web='/Volumes/Developer/WordPress/.settings/commander.sh $1 $(pwd)'
+# Start/Stop LEMP in Docker Compose
+web_lemp() {
+    case "$1" in
+        # Start Environment
+        "start") docker-compose -f $DOCKER_COMPOSE_FILE up -d;;
+
+        # Stop Environment
+        "stop") docker-compose -f $DOCKER_COMPOSE_FILE down;;
+
+        *) echo "You can 'web stop' or 'web start' only";;
+    esac
+}
+alias web="web_lemp"
+
 
 # alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup'
 alias update='brew update; brew upgrade; brew cleanup;'
