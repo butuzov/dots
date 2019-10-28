9b87580a932a667fe1c72330cb0fc7dd349c6481
Oleg Butuzov <butuzov@made.ua>
2019-10-28 17:59:04 +0200

    complete for kubectl and helm

--- bin-extentions/kubectl.sh
+++ bin-extentions/kubectl.sh
@@ -1,4 +1,9 @@
 # kubernetes related functionality
 
+alias k=kubectl
+complete -F __start_kubectl k
 . <(kubectl completion bash)
+
+alias h=helm
+complete -F __start_helm h
 . <(helm completion bash)
