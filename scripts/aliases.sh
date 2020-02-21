4fb8343b4ca30a845029c9abfd6c75a226fc951a
Oleg Butuzov <butuzov@made.ua>
2020-02-21 08:58:55 +0200

    google from terminal

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -9,11 +9,31 @@ alias dropbox="cd $DROPBOX"
 alias dp=dropbox
 
 # chrome
-# alias chrome="open -a'/Applications/Google Chrome.app' ${1}"
+alias chrome="open -a'/Applications/Google Chrome.app' $@"
 
 # Kill all the tabs in Chrome to free up memory
 # [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
 alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+
+alias chrome="open -a /Applications/Google\ Chrome.app"
+google(){
+    if [ $(echo $1 | egrep "^-[cfs]$") ]; then
+        local opt="$1"
+        shift
+    fi
+    local url="https://www.google.com/search?q=${*// /+}"
+    local app="/Applications"
+    local c="${app}/Google Chrome.app"
+    local f="${app}/Firefox.app"
+    local s="${app}/Safari.app"
+    case ${opt} in
+        "-c")   open "${url}" -a "$c";;
+        "-f")   open "${url}" -a "$f";;
+        "-s")   open "${url}" -a "$s";;
+        *)      open "${url}";;
+    esac
+}
 
 update() {
     brew update; brew upgrade; brew cleanup;
