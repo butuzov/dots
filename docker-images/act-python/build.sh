a7945a7cae6a38ba98e29d5edeaa01b9a80523b3
Oleg Butuzov <butuzov@made.ua>
2021-05-18 10:46:47 +0300

    refactor: act runners for go and python

--- docker-images/act-python/build.sh
+++ docker-images/act-python/build.sh
@@ -18,13 +18,18 @@ top() {
 
 
   touch .seen
-  for v in $(cat versions.json | jq '.[].version' --raw-output | sort -t "." -k1,1n -k2,2nr -k3,3nr); do
+  for v in $(cat versions.json | jq '.[].version' --raw-output | sort -t "." -k1,1nr -k2,2nr -k3,3nr); do
 
     minor=$(split $v "." | sed -n '2p')
+
+    if [[ "$v" =~ "alpha" ]] || [[ "$v" =~ "beta" ]] || [[ "$v" =~ "rc" ]] ; then
+      continue
+    fi
 
     if [[ $(grep $minor .seen) ]]; then
       continue
     fi
+
 
     if [[ $((--limit)) -lt 0 ]]; then
       continue
@@ -43,16 +48,17 @@ echo "FROM catthehacker/ubuntu:act-latest"                                      
 echo "ENV AGENT_TOOLSDIRECTORY=/opt/hostedtoolcache"                            >> Dockerfile
 echo "ENV RUNNER_TOOL_CACHE=/opt/hostedtoolcache"                               >> Dockerfile
 
-curl -l https://raw.githubusercontent.com/actions/go-versions/main/versions-manifest.json -o versions.json
+curl -l https://raw.githubusercontent.com/actions/python-versions/main/versions-manifest.json -o versions.json
 
-for version in $(top 8); do
-  URL=$(cat versions.json | jq --raw-output '.[].files[].download_url' | grep linux | grep $version)
+for version in $(top 5); do
+  echo $version
+  URL=$(cat versions.json | jq --raw-output '.[].files[].download_url' | grep linux | grep 20.04 | grep $version)
 
   echo ""                                                                       >> Dockerfile
-  echo "# Go ($version)"                                                        >> Dockerfile
-  echo "RUN GO_VERSION=$version \ "                                             >> Dockerfile
+  echo "# Python ($version)"                                                        >> Dockerfile
+  echo "RUN PYTHON_VERSION=$version \ "                                             >> Dockerfile
   echo " && URL=$URL \ "                                                        >> Dockerfile
-  echo ' && echo "Install Go ${GO_VERSION}" \'                                  >> Dockerfile
+  echo ' && echo "Install Python ${PYTHON_VERSION}" \'                                  >> Dockerfile
   echo ' && curl -L $URL > $(basename $URL) \'                                  >> Dockerfile
   echo ' && mkdir -p install \'                                                 >> Dockerfile
   echo ' && tar -xf $(basename $URL) -C install \'                              >> Dockerfile
