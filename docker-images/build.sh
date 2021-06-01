e8f953b248e78cae8ec5575a98c706d3d6787cc7
Oleg Butuzov <butuzov@made.ua>
2021-06-01 05:50:55 +0300

    fix: jupyter build
    + base is now 3.8 (pcycopg2)
    + added rust jupyter notebook

--- docker-images/build.sh
+++ docker-images/build.sh
@@ -2,11 +2,19 @@
 
 export DOCKER_BUILDKIT=0
 
-# docker build -t butuzov/jupyter -f jupyter-base/Dockerfile jupyter-base
+docker build -t butuzov/jupyter -f jupyter-base/Dockerfile jupyter-base
 
-# docker build -t butuzov/jupyter-julia  -f jupyter-julia/Dockerfile jupyter-julia
-# docker build -t butuzov/jupyter-golang -f jupyter-go/Dockerfile jupyter-golang
-# docker build -t butuzov/jupyter-typescript -f jupyter-typescript/Dockerfile jupyter-typescript
+docker build -t butuzov/jupyter-julia  -f jupyter-julia/Dockerfile jupyter-julia
+docker build -t butuzov/jupyter-golang -f jupyter-golang/Dockerfile jupyter-golang
+docker build -t butuzov/jupyter-typescript -f jupyter-typescript/Dockerfile jupyter-typescript
 docker build -t butuzov/jupyter-rust -f jupyter-rust/Dockerfile jupyter-rust
 
-# docker build -t butuzov/toolbox -f toolbox/Dockerfile toolbox
+docker build -t butuzov/toolbox -f toolbox/Dockerfile toolbox
+
+pushd act-go
+  ./build.sh
+popd
+
+pushd act-python
+  ./build.sh
+popd
