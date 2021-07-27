8b27be8882352afc10e8bb6cc1df163aa91fb086
Oleg Butuzov <butuzov@made.ua>
2021-07-27 17:13:52 +0300

    chores: go upgrade

--- scripts/language-go.sh
+++ scripts/language-go.sh
@@ -13,5 +13,5 @@ export GOPRIVATE=""
 
 
 # default go
-export GOPATH="$HOME/.gvm/pkgsets/go1.16.4/global"
-export GOROOT="$HOME/.gvm/gos/go1.16.4"
+export GOPATH="$HOME/.gvm/pkgsets/go1.16.6/global"
+export GOROOT="$HOME/.gvm/gos/go1.16.6"
